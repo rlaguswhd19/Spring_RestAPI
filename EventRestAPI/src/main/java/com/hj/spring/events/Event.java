c381eeaa9dd6d1348dc13a787293263c40afdd0d
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-19 10:48:31 +0900

    event

--- EventRestAPI/src/main/java/com/hj/spring/events/Event.java
+++ EventRestAPI/src/main/java/com/hj/spring/events/Event.java
@@ -2,9 +2,15 @@ package com.hj.spring.events;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Builder
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
 public class Event {
 	private Integer id;
 	private String name;
--- EventRestAPI/src/test/java/com/hj/spring/events/EventTest.java
+++ EventRestAPI/src/test/java/com/hj/spring/events/EventTest.java
@@ -8,7 +8,24 @@ public class EventTest {
 	
 	@Test
 	public void builder() {
-		Event event = Event.builder().build();
+		Event event = Event.builder()
+				.name("KHJ")
+				.description("REST API")
+				.build();
 		assertThat(event).isNotNull();
+	}
+	
+	@Test
+	public void javaBean() {
+		String name = "Event";
+		String description = "Spring";
+		
+		Event event = new Event();
+		event.setName("Event");
+		event.setDescription("Spring");
+		
+		assertThat(event.getName()).isEqualTo(name);
+		assertThat(event.getDescription()).isEqualTo(description);
+		
 	}
 }
