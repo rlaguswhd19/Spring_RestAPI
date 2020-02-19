e1990d865916f66edfdf4b7f77326faecb3d7f1c
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-19 16:34:17 +0900

    EventControllerTest

--- EventRestAPI/src/main/java/com/hj/spring/events/Event.java
+++ EventRestAPI/src/main/java/com/hj/spring/events/Event.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
+@Getter @Setter @EqualsAndHashCode(of = "id") //Equals랑 Hashcode를 비교할때  id만 사용한다.
 public class Event {
 	private Integer id;
 	private String name;
--- EventRestAPI/src/test/java/com/hj/spring/events/EventControllerTest.java
+++ EventRestAPI/src/test/java/com/hj/spring/events/EventControllerTest.java
@@ -1,7 +1,11 @@
 package com.hj.spring.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class EventControllerTest {
@@ -19,12 +25,31 @@ public class EventControllerTest {
 	@Autowired
 	MockMvc mocMvc;
 	
+	@Autowired
+	ObjectMapper objectMapper;
+	
 	@Test
 	public void createEvent() throws Exception {
+		Event event = Event.builder()
+				.name("Srping")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2020, 02, 19, 14, 19))
+				.closeEnrollmentDateTime(LocalDateTime.of(2020, 02, 20, 14, 19))
+				.beginEventDateTime(LocalDateTime.of(2020, 02, 21, 14, 19))
+				.endEventDateTime(LocalDateTime.of(2020, 02, 22, 14, 19))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.location("대전 현지의 러브하우스")
+				.build();
+				
 		mocMvc.perform(post("/api/events/")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaTypes.HAL_JSON)
+				.content(objectMapper.writeValueAsString(event))
 				) 
-			.andExpect(status().isCreated());
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("id").exists());
 	}
 }
